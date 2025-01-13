67a610f2b0cbbfcc1e7640489cc4405dab49fa57
Dmitry <fedyashevdm@gmail.com>
2025-01-13 10:31:35 +0300

    second commit

---
+++ src/main/java/com/tdl/tdl_bank/TdlBankApplication.java
@@ -0,0 +1,13 @@
+package com.tdl.tdl_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdlBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdlBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdl/tdl_bank/TdlBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdl.tdl_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdlBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
